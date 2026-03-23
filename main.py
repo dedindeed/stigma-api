4499c2871e2da1f517235487e7f2ba564d6d2b6b
dedindeed <epiksbois2@gmail.com>
2026-03-23 19:04:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-from flask import Flask, jsonify
-import requests
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Link to your raw GitHub JSON
-DB_URL = "https://raw.githubusercontent.com/dedindeed/stigma-api/refs/heads/main/database.json"
+# This variable holds the script until Roblox asks for it
+queued_script = ""
 
-@app.route('/')
-def home():
-    return "Stigma API is Running"
+@app.route('/api/execute', methods=['POST'])
+def execute():
+    global queued_script
+    data = request.json
+    queued_script = data.get("script", "")
+    print(f"Received Script: {queued_script}")
+    return jsonify({"status": "ok"}), 200
 
-@app.route('/api/scripts')
-def get_scripts():
-    try:
-        response = requests.get(DB_URL)
-        return jsonify(response.json())
-    except:
-        return jsonify({"error": "Failed to fetch database"})
+@app.route('/api/get_script', methods=['GET'])
+def get_script():
+    global queued_script
+    # Send the script to Roblox and then clear the queue
+    temp = queued_script
+    queued_script = "" 
+    return jsonify({"script": temp}), 200
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
